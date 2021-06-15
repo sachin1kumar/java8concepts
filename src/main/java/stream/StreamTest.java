5fdf680de3fe1fd867ce394cd35fa76c139d1c5e
Sachin <sachinkmr375@gmail.com>
2021-06-15 18:58:06 +0530

    stream api notes added

--- src/main/java/stream/StreamTest.java
+++ src/main/java/stream/StreamTest.java
@@ -2,16 +2,15 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class StreamTest {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3);
 
-        Stream<Integer> stream = list.parallelStream();
-
-        stream.forEach(System.out::println);
-        //stream.forEach(System.out::println);
+        list.stream()
+                .filter(num -> num % 2 == 0) /*if this is not true, thenit will remove the num from conveyor belt and return false.
+                For each  will not be called in this case*/
+                .forEach(num -> System.out.println(num));
     }
 }
